17b33614d1977652a464552068b1a6609a2eba01
rolandvarga <roland.varga.can@gmail.com>
2022-07-23 10:08:59 -0400

    update header color

--- src/lib.rs
+++ src/lib.rs
@@ -152,7 +152,7 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
         .split(f.size());
 
     let selected_style = Style::default().add_modifier(Modifier::REVERSED);
-    let normal_style = Style::default().bg(Color::Blue);
+    let normal_style = Style::default().bg(Color::Green);
     let header_cells = ["FILE_NAME", "TABLES"]
         .iter()
         .map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
